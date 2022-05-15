45e79a77a4487f74c3fb6a552a1f4b264acd683a
enessimsek-4318 <101976848+enessimsek-4318@users.noreply.github.com>
2022-05-15 11:43:03 +0300

    Student_Information
    This study that I have done has been done for learning purposes.

---
+++ Ogrenci_Bilgileri/Ogrenci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrenci_Bilgileri
+{
+    class Ogrenci
+    {
+        private int ogrenciNo;
+        private string ogrenciIsmi;
+        private string ogrenciSoyismi;
+        private double vize1;
+        private double vize2;
+        private double final;
+        private string okulIsmi;
+
+
+        public void ogrenciBilgileriGoster()
+        {
+            Console.WriteLine("Ogrenci No: " + ogrenciNo);
+            Console.WriteLine("Ogrenci Adı: " + ogrenciIsmi);
+            Console.WriteLine("Ogrenci Soyadı: " + ogrenciSoyismi);
+            Console.WriteLine("Ogrenci Vize-1 Notu: " + vize1);
+            Console.WriteLine("Ogrenci Vize-2 Notu: " + vize2);
+            Console.WriteLine("Ogrenci Final Notu: " + final);
+            Console.WriteLine("Okul Adı: " + okulIsmi);
+        }
+        public Ogrenci(int No, string ad, string soyad, double v1, double v2, double fin, string okulad) // Yapıcı Metod Olarak Geçmektedir. Kısa Yolu ctor yazıp 2 kere tab 
+        {
+            ogrenciNo = No;
+            ogrenciIsmi = ad;
+            ogrenciSoyismi = soyad;
+            vize1 = v1;
+            vize2 = v2;
+            final = fin;
+            okulIsmi = okulad;
+        }
+        public double ortalamabul()
+        {
+            double ortalama = (vize1 * 0.2) + (vize2 * 0.2) + (final * 0.6);
+            return ortalama;
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Okul Adı: " + okulIsmi);
+        }
+
+    }
+}
---
+++ Ogrenci_Bilgileri/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ogrenci_Bilgileri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(1605061164, "Enes Muhammed", "Şimşek", 50, 65, 84, "Yıldırım Beyazıt Üniversitesi");
+            while (kontrol)
+            {
+                Console.WriteLine("Enes Şimşek OBS sistemine Hoşgeldiniz:");
+                Console.WriteLine("Öğrenci Bilgileri  :1\nÖğrenci Ortalaması :2\nÖğrencinin Okulu   :3\nÇıkış              :4\nSeçiminizi Yapınız");
+                int secim = Convert.ToInt32(Console.ReadLine());
+                if (secim == 1)
+                {
+                    ogrenci1.ogrenciBilgileriGoster();
+                }
+                else if (secim == 2)
+                {
+                    double ort =ogrenci1.ortalamabul();
+                    Console.WriteLine("Ortalamanız: " + ort);
+                    if (ort<50)
+                    {
+                        Console.WriteLine("Dersten Kaldınız");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dersten Geçtiniz");
+                    }
+                }
+                else if (secim == 3)
+                {
+                    ogrenci1.okulGetir();
+                }
+                else if (secim == 4)
+                {
+                    kontrol = false;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı Tuşlama Yaptınız");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
